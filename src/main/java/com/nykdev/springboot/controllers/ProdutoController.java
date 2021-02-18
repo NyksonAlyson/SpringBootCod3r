fce085474d4f10639816d8d28c72ac2ed7b5bd91
nyksonalyson <nyksonalison@hotmail.com>
2021-02-18 15:36:11 -0300

    Excluindo produto por ID

--- src/main/java/com/nykdev/springboot/controllers/ProdutoController.java
+++ src/main/java/com/nykdev/springboot/controllers/ProdutoController.java
@@ -5,6 +5,7 @@ import java.util.Optional;
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -45,6 +46,11 @@ public class ProdutoController {
 	public Produto alterarProduto(@Valid Produto produto) {
 		return produtoRepository.save(produto);
 		 
+	}
+	// METODO PARA DELETAR PRODDUTO POR ID
+	@DeleteMapping(path = "/{id}")
+	public void ExcluirProduto(@PathVariable int id) {
+		produtoRepository.deleteById(id);
 	}
 
 }
